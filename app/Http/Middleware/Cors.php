5d8f7f2680f4853639b3ff08788a84f911199d48
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-05-27 15:38:42 +0200

    Arreglada autenticación con Sanctum

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations')
+            ->header('Access-Control-Allow-Credentials', 'true');
+    }
+}
